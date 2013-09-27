e7399b393da57ae2839eb864915caa27483c7e5c
Suny Gao <suny.gao@gmail.com>
2013-09-26 18:00:42 -0700

    adding some projects

--- www/php/view/index.php
+++ www/php/view/index.php
@@ -1,22 +1,70 @@
 <?php include DIR_TMPL.'/header.php'; ?>
 
 	<ul id="projects">
+
+		<!-- AMERICA'S BACKYARD -->
 		<li id="americas_backyard">
 			<ul class="images">
 				<li>
 					<img src="media/images/projects/americasbackyard/homepage_desktop.jpg" />
 				</li>
 				<li>
-					<img src="" />
+					<img src="media/images/projects/americasbackyard/homepage_desktop_opening.jpg" />
+				</li>
+				<li>
+					<img src="media/images/projects/americasbackyard/southbbq_intro.jpg" />
+				</li>
+				<li>
+					<img src="media/images/projects/americasbackyard/open_hotspot.jpg" />
+				</li>
+				<li>
+					<img src="media/images/projects/americasbackyard/iphone.jpg" />
 				</li>
 			</ul>
 			<div class="description">
 				<h2>America's Backyard</h2>
-				<h3>La Brea Bakery <a href="http://labreabakery.com/americas-backyard/" target="_blank">Visit Site <span></span></a></h3>
-				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</p>
+				<h3>La Brea Bakery <span>|</span> <a href="http://labreabakery.com/americas-backyard/" target="_blank">Visit Site <span></span></a></h3>
+				<p>La Brea Bakery celebrates summer grilling with America's Backyard, a microsite featuring 6 tabletops showcasing tips, recipes, and bread pairings. Site features scrolling animations, iPad and mobile support.</p>
+
+				<p>Tools: HTML, CSS transforms, Javascript, PHP</p>
 			</div>
 		</li>
+
+		<!-- EVB.COM -->
+		<li id="evb">
+			<ul class="images">
+				<li>
+					<img src="media/images/projects/evb/evb_home.jpg" />
+				</li>
+				<li>
+					<img src="media/images/projects/evb/evb_clients.jpg" />
+				</li>
+				<li>
+					<img src="media/images/projects/evb/evb_work.jpg" />
+				</li>
+				<li>
+					<img src="media/images/projects/evb/evb_about.jpg" />
+				</li>
+				<li>
+					<img src="media/images/projects/evb/evb_mobile.jpg" />
+				</li>
+			</ul>
+			<div class="description">
+				<h2>EVB.com</h2>
+				<h3>Evolution Bureau <span>|</span> <a href="http://evb.com" target="_blank">Visit Site <span></span></a></h3>
+				<p>Launched in June 2012, EVB's new site showcases the Bureau's work, people and philosophy. Built on top of a custom Wordpress CMS, the site is completely responsive; it seamlessly scales to multiple desktop resolutions as well as mobile devices. It also uses HTML 5 features such as video, SVG and CSS3, with fallbacks for IE7 and 8.</p>
+
+				<p>EVB.com was featured as the FWA's Mobile Site of the Day on <a href="http://www.thefwa.com/mobile/evolution-bureau-evb-website" target="_blank">July 2, 2012</a>.</p>
+
+				<p>Tools: HTML 5 video, CSS transforms, CSS animations, SVG animations, Responsive design, Javascript, PHP, Wordpress</p>
+
+
+			</div>
+		</li>
+
+
 	</ul>	
 </div>
 
 <?php include DIR_TMPL.'/footer.php'; ?>
+
