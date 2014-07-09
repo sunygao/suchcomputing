1e44505ad06e78a7cbc63e9b21af4fcc8a6b7542
sgao <sgao@droga5.com>
2014-07-09 13:41:06 -0400

    update description

--- www/php/view/about.php
+++ www/php/view/about.php
@@ -1,6 +1,8 @@
 <?php include DIR_TMPL.'/header.php'; ?>
 
 <h2>About</h2>
-<p>Brooklyn-based front end developer currently freelancing at Droga5.</p>
+<p>Brooklyn-based front end developer/nice person currently freelancing at <a href="http://www.droga5.com" target="_blank">Droga5</a>.</p>
+
+
 
 <?php include DIR_TMPL.'/footer.php'; ?>
